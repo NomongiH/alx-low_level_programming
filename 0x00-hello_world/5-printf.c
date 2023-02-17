f196e61b07a27bdcc4cb9199e92ccdbb960ea84f
Nomongi Hlatshwayo <nomongih@gmail.com>
2023-02-17 06:37:38 -0800

    C Programming - Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Using printf fucnction to output a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun(0);
+}
+
